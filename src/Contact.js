8158123962db959703c65c544f62619bf1928f45
Matthew Leng <mleng89@gmail.com>
2021-04-09 18:34:04 -0400

    Edit: Contact links

--- src/Contact.js
+++ src/Contact.js
@@ -9,7 +9,11 @@ export default function Contact() {
 				<div className='footer_item'>
 					<a href='https://github.com/Mleng89'>
 						<a className='github'>
-							<img src='/github.svg' width='250rem' height='50rem' />
+							<img
+								src='https://raw.githubusercontent.com/Mleng89/navbartemplate/5ca032ca1b53f9adcb789e2c1721ab430e62ceda/public/github.svg'
+								width='250rem'
+								height='50rem'
+							/>
 						</a>
 					</a>
 				</div>
@@ -17,19 +21,27 @@ export default function Contact() {
 				<div className='footer_item'>
 					<a href='https://www.linkedin.com/in/matthew-leng/'>
 						<a>
-							<img src='/linkedin.svg' width='250rem' height='50rem' />
+							<img
+								src='https://raw.githubusercontent.com/Mleng89/navbartemplate/5ca032ca1b53f9adcb789e2c1721ab430e62ceda/public/linkedin.svg'
+								width='250rem'
+								height='50rem'
+							/>
 						</a>
 					</a>
 				</div>
 				<div className='footer_item'>
 					<a href='https://twitter.com/matthewleng'>
 						<a>
-							<img src='/twitter.svg' width='250rem' height='50rem' />
+							<img
+								src='https://raw.githubusercontent.com/Mleng89/navbartemplate/5ca032ca1b53f9adcb789e2c1721ab430e62ceda/public/twitter.svg'
+								width='250rem'
+								height='50rem'
+							/>
 						</a>
 					</a>
 				</div>
 				<div className='footer_item'>
-					<a href='mailto:mleng89@gmail.com' className='email_link'>
+					<a href='mailto:mleng89@gmail.com' className='contact_links'>
 						Contact me
 					</a>
 				</div>
