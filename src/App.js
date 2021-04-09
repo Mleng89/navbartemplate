89d9497e3298443b17e5444920025aeff4cc3cdb
Matthew Leng <mleng89@gmail.com>
2021-04-09 16:03:31 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<nav className='navbar'>
+					<div className='brand-title'>Brand name</div>
+					<div className='navbar-links'>
+						<ul>
+							<li>
+								<a>
+									<NavLink activeClassName='is-active' exact={true} to='/'>
+										Home
+									</NavLink>
+								</a>
+							</li>
+							<li>
+								<a>
+									<NavLink
+										activeClassName='is-active'
+										exact={true}
+										to='/contact'
+									>
+										Contact
+									</NavLink>
+								</a>
+							</li>
+							<li>
+								<a>
+									<NavLink activeClassName='is-active' exact={true} to='/about'>
+										About
+									</NavLink>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
