7d9d2dd3c8b5d5ca503cdc7cfada97992d2f93ab
Matthew Leng <mleng89@gmail.com>
2021-04-09 17:25:03 -0400

    Added: Routes and css for mobile drawer navbar

---
+++ src/AllRoutes.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { Switch, useLocation, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function AllRoutes({ hideMenu }) {
+	let location = useLocation();
+	useEffect(() => {
+		hideMenu();
+	}, [location]);
+
+	return (
+		<Switch>
+			<Route path='/about' component={About}></Route>
+			<Route path='/contact' component={Contact}></Route>
+			<Route path='/' component={Home}></Route>
+		</Switch>
+	);
+}
+
+export default AllRoutes;
--- src/App.js
+++ src/App.js
@@ -1,42 +1,76 @@
 import './App.css';
+import { useRef, useEffect } from 'react';
 import { BrowserRouter, NavLink } from 'react-router-dom';
-
+import AllRoutes from './AllRoutes';
 function App() {
+	const navbarLinks = useRef(null);
+	const handleNavBar = (e) => {
+		// console.log('what is my navbarLinks', navbarLinks.current);
+		navbarLinks.current.classList.toggle('menu-collapse');
+	};
+	const hideNav = () => {
+		// console.log('this is inside hideNav function');
+		if (!navbarLinks.current.classList.contains('menu-collapse'))
+			navbarLinks.current.classList.add('menu-collapse');
+	};
+
 	return (
 		<div className='App'>
 			<BrowserRouter>
 				<nav className='navbar'>
 					<div className='brand-title'>Brand name</div>
-					<div className='navbar-links'>
-						<ul>
-							<li>
-								<a>
-									<NavLink activeClassName='is-active' exact={true} to='/'>
-										Home
-									</NavLink>
-								</a>
+					<button
+						className='toggle-button'
+						onClick={(e) => {
+							handleNavBar(e);
+						}}
+					>
+						<span className='bar'></span>
+						<span className='bar'></span>
+						<span className='bar'></span>
+					</button>
+					<div ref={navbarLinks} className='navbar-links menu-collapse'>
+						<ul className='links-list'>
+							<li className='nav-item'>
+								<NavLink
+									className='nav-link'
+									activeClassName='is-active'
+									exact={true}
+									to='/'
+								>
+									Home
+								</NavLink>
 							</li>
-							<li>
-								<a>
-									<NavLink
-										activeClassName='is-active'
-										exact={true}
-										to='/contact'
-									>
-										Contact
-									</NavLink>
-								</a>
+							<li className='nav-item'>
+								<NavLink
+									className='nav-link'
+									activeClassName='is-active'
+									exact={true}
+									to='/contact'
+								>
+									Contact
+								</NavLink>
 							</li>
-							<li>
-								<a>
-									<NavLink activeClassName='is-active' exact={true} to='/about'>
-										About
-									</NavLink>
-								</a>
+							<li className='nav-item'>
+								<NavLink
+									className='nav-link'
+									activeClassName='is-active'
+									exact={true}
+									to='/about'
+								>
+									About
+								</NavLink>
 							</li>
 						</ul>
 					</div>
 				</nav>
+				<div>
+					<AllRoutes
+						hideMenu={() => {
+							hideNav();
+						}}
+					></AllRoutes>
+				</div>
 			</BrowserRouter>
 		</div>
 	);
